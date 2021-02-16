58ecc41eb6a1d4ed9e74aeaec605affc260529ca
Megan Kuo <megan.kuo@live.com>
2021-02-16 10:22:46 -0800

    complete starter files

--- src/SortUtil.ts
+++ src/SortUtil.ts
@@ -1,5 +1,25 @@
-import { NumberGroup } from "./NumberGroup";
-import { CharacterGroup } from "./CharacterGroup";
-import { LinkedListGroup } from "./LinkedListGroup";
+import { ISortable } from "./ISortable";
 
-export class SortUtil {}
+export class SortUtil {
+  collection: ISortable;
+
+  constructor(collection: ISortable) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+    let isSorted = false;
+    let lastUnsorted = length - 1;
+    while (!isSorted) {
+      isSorted = true;
+      for (let i = 0; i < lastUnsorted; i++) {
+        if (this.collection.compare(i, i + 1)) {
+          this.collection.swap(i, i + 1);
+          isSorted = false;
+        }
+      }
+    }
+    lastUnsorted--;
+  }
+}
